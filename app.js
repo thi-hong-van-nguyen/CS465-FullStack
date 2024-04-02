60506cc4f57fcad7d58462871f06a3620fae453a
thi-hong-van-nguyen <thinguyen.webdev@gmail.com>
2024-04-01 22:42:16 -0500

    completed module5

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ var logger = require("morgan");
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
+var travelDetailsRouter = require("./app_server/routes/travelDetails");
 var roomsRouter = require("./app_server/routes/rooms");
 var mealsRouter = require("./app_server/routes/meals");
 var newsRouter = require("./app_server/routes/news");
 var aboutRouter = require("./app_server/routes/about");
 var contactRouter = require("./app_server/routes/contact");
+var apiRouter = require("./app_api/routes/index");
 
 var handlebars = require("hbs");
+
+require("./app_api/models/db");
 
 var app = express();
 
@@ -34,11 +38,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
+app.use("/travel", travelDetailsRouter);
 app.use("/rooms", roomsRouter);
 app.use("/meals", mealsRouter);
 app.use("/news", newsRouter);
 app.use("/about", aboutRouter);
 app.use("/contact", contactRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,15 +1,67 @@
-var fs = require("fs");
-var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+// var fs = require("fs");
+// var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+	method: "GET",
+	headers: {
+		Accept: "application/json",
+	},
+};
 
 /* GET travel view */
-const travel = (req, res) => {
-	res.render("travel", {
-		title: "Travlr Getaways",
-		trips,
-		selectedTravel: true,
-	});
+const travel = async (req, res, next) => {
+	await fetch(tripsEndpoint, options)
+		.then((res) => res.json())
+		.then((json) => {
+			let message = null;
+			if (!(json instanceof Array)) {
+				message = "API lookup error";
+				json = [];
+			} else {
+				if (!json.length) {
+					message = "No trips exist in our database";
+				}
+			}
+
+			res.render("travel", {
+				title: "Travlr Getaways",
+				trips: json,
+				selectedTravel: true,
+				message,
+			});
+		})
+		.catch((err) => res.status(500).send(err.message));
+};
+
+/* GET travel details view */
+const travelDetails = async (req, res, next) => {
+	const tripCode = req.params.tripCode;
+	const tripDetailsUrl = tripsEndpoint + "/" + tripCode;
+
+	await fetch(tripDetailsUrl, options)
+		.then((res) => res.json())
+		.then((json) => {
+			let message = null;
+			if (!(json instanceof Array)) {
+				message = "API lookup error";
+				json = [];
+			} else {
+				if (!json.length) {
+					message = "No such trips exist in our database";
+				}
+			}
+
+			res.render("travelDetails", {
+				title: "Travlr Getaways",
+				trips: json,
+				message,
+			});
+		})
+		.catch((err) => res.status(500).send(err.message));
 };
 
 module.exports = {
 	travel,
+	travelDetails,
 };
